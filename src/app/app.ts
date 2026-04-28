5d864f5465b884a9242107f9c8a41938d6a0de55
Anna Zach <anza2501@student.miun.se>
2026-04-28 17:04:37 +0200

    Lägger till sortering av kurser i tabellen

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CourseService } from './services/course.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+
+type SortKey = 'code' | 'coursename' | 'progression';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -15,4 +19,30 @@ export class App {
   courses = toSignal(this.courseService.getCourses(), {
     initialValue: []
   });
+
+  sortKey = signal<SortKey>('code');
+  sortDirection = signal<'asc' | 'desc'>('asc');
+
+  sortedCourses = computed(() => {
+    const data = [...this.courses()];
+
+    return data.sort((a, b) => {
+      const key = this.sortKey();
+
+      if (a[key] < b[key]) return this.sortDirection() === 'asc' ? -1 : 1;
+      if (a[key] > b[key]) return this.sortDirection() === 'asc' ? 1 : -1;
+      return 0;
+    });
+  });
+
+  setSort(key: SortKey) {
+    if (this.sortKey() === key) {
+      this.sortDirection.set(
+        this.sortDirection() === 'asc' ? 'desc' : 'asc'
+      );
+    } else {
+      this.sortKey.set(key);
+      this.sortDirection.set('asc');
+    }
+  }
 }
